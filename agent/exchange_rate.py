b0928bd78c0bf1fb4551264b4531433ee482a8c7
developsomethingcool <pometunalexander@gmail.com>
2025-05-19 23:32:49 +0200

    Initialized general structure of a proj

---
+++ agent/exchange_rate.py
@@ -0,0 +1,10 @@
+import requests
+import os 
+from dotenv import load_dotenv, dotenv_values 
+
+load_dotenv() 
+print(os.getenv("PRIVATE_KEY"))
+
+def converter(curency_one, currency_two):
+    pass
+
