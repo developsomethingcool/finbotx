2fdfecef8f70befb62f4f81aab736a0643cc9c06
developsomethingcool <pometunalexander@gmail.com>
2025-05-23 19:46:12 +0200

    Created langchain based tool

--- agent/tools.py
+++ agent/tools.py
@@ -2,6 +2,7 @@ from langchain_core.tools import tool
 from exchange_rate import *
 
 @tool
-def exchange_rate_tool(from_curr: str, to_curr: str) -> str:
+def exchange_rate_tool(from_curr: str, to_curr: str, amount: int) -> str:
     """Get an up-to-date exchange rate for two currencies """
-    pass
+    return converter(from_curr, to_curr, amount)
+
